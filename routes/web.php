71d2d3b989cc2d4bba1c5e2b3743d5db6ac26b1a
kutteai <kutteai978@gmail.com>
2025-08-18 07:36:41 +0100

    Add webhook routes to web.php as temporary solution

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PaymentCallbackController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Paystack webhook route - temporary placement
+Route::post('/payments/callback', [PaymentCallbackController::class, 'handlePaymentCallback']);
+Route::post('/api/v1/payments/callback', [PaymentCallbackController::class, 'handlePaymentCallback']);
