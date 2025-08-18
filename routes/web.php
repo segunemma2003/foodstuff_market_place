1d35d633f41f9eccec67a280ee81f65dccded4cb
kutteai <kutteai978@gmail.com>
2025-08-18 09:18:52 +0100

    a

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PaymentCallbackController;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 // Paystack webhook route - following exact documentation
-Route::post('/webhook/paystack', [PaymentCallbackController::class, 'handleWebhook']);
+Route::post('/webhook/paystack', [PaymentCallbackController::class, 'handleWebhook']) ->withoutMiddleware([VerifyCsrfToken::class]);
 Route::post('/payment-callback', [PaymentCallbackController::class, 'handleWebhook']);
 Route::post('/payments/callback', [PaymentCallbackController::class, 'handleWebhook']);
 Route::post('/api/v1/payments/callback', [PaymentCallbackController::class, 'handleWebhook']);
